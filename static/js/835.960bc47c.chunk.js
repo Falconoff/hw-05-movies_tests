5f1d4594f9f3153fcab1f6a920ad3534f719f629
Falconoff <68744408+Falconoff@users.noreply.github.com>
2022-02-14 16:00:16 +0000

    Deploying to gh-pages from @ Falconoff/hw-05-movies_tests@556680aa916dae834e11cdf25a2a8d59ae3f8fdc 🚀

---
+++ static/js/835.960bc47c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[835],{2835:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)("h1",{children:"Contacts"})}}}]);
+//# sourceMappingURL=835.960bc47c.chunk.js.map
